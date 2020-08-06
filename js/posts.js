bb3718bdfd3c007dab525c5f00db5d96fd179522
dancan-sandys <dancan.oruko96@gmail.com>
2020-08-06 10:39:17 +0300

    creat cards for each post to styles them individually

--- js/posts.js
+++ js/posts.js
@@ -4,11 +4,14 @@ fetch(`https://www.heynutritionlady.com/wp-json/wp/v2/posts`)
 })
 .then(function(post){
     for (var i =0; i<10 ; i++){
- $(`#posts`).append(`<h1>${post[i].title.rendered}</h1>`)
- $(`#posts`).append(post[i].content.rendered)
- $(`#posts`).append(post[i].excerpt.rendered)
- $(`#posts`).append(post[i].yoast_head)
- $(`#posts`).append(`<hr>`)
-    }
+    
+    $(`#posts`).append(`<div class= "card">`)
+    $(`#posts`).append(`<h1>${post[i].title.rendered}</h1>`)
+    $(`#posts`).append(post[i].content.rendered)
+    $(`#posts`).append(post[i].excerpt.rendered)
+    $(`#posts`).append(post[i].yoast_head)
+    
+    $(`#posts`).append(`<div>`)
+        }
 })
 
